d9182f354f8dac79f2fdfc9738ec6f2485090a35
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-03-24 19:26:37 +0000

    first commit

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import Test.Partest (someFunc)
+
+main :: IO ()
+main = someFunc
---
+++ src/Test/Partest.hs
@@ -0,0 +1,6 @@
+module Test.Partest
+    ( someFunc
+    ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
