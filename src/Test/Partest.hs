412f7828b1059edb1897a86baec5fc464040f22c
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-04-06 03:38:59 +0100

    feat: add WACC types as testing examples

--- src/Test/Partest.hs
+++ src/Test/Partest.hs
@@ -129,44 +129,48 @@ newtype Sym = Sym String
 data Expr = Choices [Sym] | Seqs [Sym] | Term String
   deriving (Show, Ord, Eq)
 
-data Rule = Rule Sym Expr
+data Rule = Rule Sym Expr RuleType
+  deriving (Show, Ord, Eq)
+
+-- a rule is either defined by user or extracted from the grammar
+data RuleType = Defined | Extracted
   deriving (Show, Ord, Eq)
 
 singleTerm :: Rule
-singleTerm = Rule (Sym "singleTerm") (Term "[singleTerm]")
+singleTerm = Rule (Sym "singleTerm") (Term "[singleTerm]") Defined
 
 seqTerm1 :: Rule
-seqTerm1 = Rule (Sym "seqTerm1") (Term "[seqTerm1]")
+seqTerm1 = Rule (Sym "seqTerm1") (Term "[seqTerm1]") Defined
 seqTerm2 :: Rule
 
 seqTerm :: Rule
-seqTerm2 = Rule (Sym "seqTerm2") (Term "[seqTerm2]")
+seqTerm2 = Rule (Sym "seqTerm2") (Term "[seqTerm2]") Defined
 
-seqTerm = Rule (Sym "seqTerm") $ Seqs [Sym "seqTerm1", Sym "seqTerm2"]
+seqTerm = Rule (Sym "seqTerm") (Seqs [Sym "seqTerm1", Sym "seqTerm2"]) Defined
 
 choicesTerm1 :: Rule
-choicesTerm1 = Rule (Sym "choicesTerm1") (Term "[choicesTerm1]")
+choicesTerm1 = Rule (Sym "choicesTerm1") (Term "[choicesTerm1]") Defined
 
 choicesTerm2 :: Rule
-choicesTerm2 = Rule (Sym "choicesTerm2") (Term "[choicesTerm2]")
+choicesTerm2 = Rule (Sym "choicesTerm2") (Term "[choicesTerm2]") Defined
 
 choicesTerm :: Rule
-choicesTerm = Rule (Sym "choicesTerm") $ Choices [Sym "choicesTerm1", Sym "choicesTerm2"]
+choicesTerm = Rule (Sym "choicesTerm") (Choices [Sym "choicesTerm1", Sym "choicesTerm2"]) Defined
 
 recursiveTerm1' :: Rule
-recursiveTerm1' = Rule (Sym "recursiveTerm1'") (Term "[recursiveTerm1]")
+recursiveTerm1' = Rule (Sym "recursiveTerm1'") (Term "[recursiveTerm1]") Defined
 
 recursiveTerm2' :: Rule
-recursiveTerm2' = Rule (Sym "recursiveTerm2'") (Term "[recursiveTerm2]")
+recursiveTerm2' = Rule (Sym "recursiveTerm2'") (Term "[recursiveTerm2]") Defined
 
 recursiveTerm :: Rule
-recursiveTerm = Rule (Sym "recursiveTerm") $ Choices [Sym "recursiveTerm1", Sym "recursiveTerm2"]
+recursiveTerm = Rule (Sym "recursiveTerm") (Choices [Sym "recursiveTerm1", Sym "recursiveTerm2"])  Defined
 
 recursiveTerm1 :: Rule
-recursiveTerm1 = Rule (Sym "recursiveTerm1") (Choices [Sym "recursiveTerm", Sym "recursiveTerm1'"])
+recursiveTerm1 = Rule (Sym "recursiveTerm1") (Choices [Sym "recursiveTerm", Sym "recursiveTerm1'"]) Defined
 
 recursiveTerm2 :: Rule
-recursiveTerm2 = Rule (Sym "recursiveTerm2") (Choices [Sym "recursiveTerm", Sym "recursiveTerm2'"])
+recursiveTerm2 = Rule (Sym "recursiveTerm2") (Choices [Sym "recursiveTerm", Sym "recursiveTerm2'"]) Defined
 
 
 compile :: [Rule] -> [(BNF, Integer, [Integer])]
@@ -184,15 +188,15 @@ assignIds :: [Rule] -> Map Sym Integer
 assignIds rs = fromList $ zip (map symbol rs) [0 ..]
 
 symbol :: Rule -> Sym
-symbol (Rule s _) = s
+symbol (Rule s _ _) = s
 
 compileExpr :: Rule -> BNF
-compileExpr (Rule sym (Choices _)) = BChoices sym
-compileExpr (Rule sym (Seqs _)) = BSeqs sym
-compileExpr (Rule sym (Term s)) = BTerm s sym
+compileExpr (Rule sym (Choices _) _) = BChoices sym
+compileExpr (Rule sym (Seqs _) _) = BSeqs sym
+compileExpr (Rule sym (Term s) _) = BTerm s sym
 
 compileEdge :: Rule -> Map Sym Integer -> [Integer]
-compileEdge (Rule _ expr) m = map (m !) (flattenExpr expr)
+compileEdge (Rule _ expr _) m = map (m !) (flattenExpr expr)
 
 flattenExpr :: Expr -> [Sym]
 flattenExpr (Seqs ss) = ss
@@ -201,7 +205,7 @@ flattenExpr (Term _) = []
 
 
 compileId :: Rule -> Map Sym Integer -> Integer
-compileId (Rule s _) m = m ! s
+compileId (Rule s _ _) m = m ! s
 
 data Res = RTerm String Sym | RSeq Sym | RChoices Sym
   deriving (Show)
@@ -231,17 +235,6 @@ gen (BChoices sym, _, xs) m depth
       res <- gen (m ! j) m (depth - 1)
       return $ Node (RChoices sym) [res]
 
-
-genAll :: [BNF'] -> Int -> Gen (Tree Res)
-genAll bs i = do
-  j <- elements bs
-  gen j ms i
-
-g' :: Int -> IO ()
-g' x = do 
-  xs <- sample' $ genAll es x
-  mapM_ (putStrLn . showTree) xs
-
 rules :: [Rule]
 rules =
   [ singleTerm
@@ -264,3 +257,88 @@ es' = compile rules
 ms' :: Map Integer BNF'
 ms' = inverseMap es'
 
+int :: Rule
+int = Rule (Sym "int") (Term "int") Extracted
+
+bool :: Rule
+bool = Rule (Sym "bool") (Term "bool") Extracted
+
+char :: Rule
+char = Rule (Sym "char") (Term "char") Extracted
+
+string :: Rule
+string = Rule (Sym "string") (Term "string") Extracted
+
+baseType :: Rule
+baseType = Rule (Sym "baseType") (Choices [Sym "int", Sym "bool", Sym "char", Sym "string"]) Defined
+
+pair :: Rule
+pair = Rule (Sym "pair") (Term "pair") Extracted
+
+leftParam :: Rule
+leftParam = Rule (Sym "leftParam") (Term "(") Extracted
+
+comma :: Rule
+comma = Rule (Sym "comma") (Term ",") Extracted
+
+rightParam :: Rule
+rightParam = Rule (Sym "rightParam") (Term ")") Extracted
+
+leftSquareBracket :: Rule
+leftSquareBracket = Rule (Sym "leftSquareBracket") (Term "[") Extracted
+
+rightSquareBracket :: Rule
+rightSquareBracket = Rule (Sym "rightSquareBracket") (Term "]") Extracted
+
+ttype :: Rule
+ttype = Rule (Sym "ttype") (Choices [Sym "baseType", Sym "arrayType", Sym "pairType"]) Defined
+
+arrayType :: Rule
+arrayType = Rule (Sym "arrayType") (Seqs [Sym "ttype", Sym "leftSquareBracket", Sym "rightSquareBracket"]) Defined
+
+pairType :: Rule
+pairType = Rule (Sym "pairType") (Seqs [Sym "pair", Sym "leftParam", Sym "pairElemType", Sym "comma", Sym "pairElemType", Sym "rightParam"]) Defined
+
+pairElemType :: Rule
+pairElemType = Rule (Sym "pairElemType") (Choices [Sym "baseType", Sym "arrayType", Sym "pair"]) Defined
+
+rules' :: [Rule]
+rules' =
+  [ int
+  , bool
+  , char
+  , string
+  , baseType
+  , pair
+  , leftParam
+  , comma
+  , rightParam
+  , leftSquareBracket
+  , rightSquareBracket
+  , ttype
+  , arrayType
+  , pairType
+  , pairElemType
+  ]
+
+defined :: Rule -> Bool
+defined (Rule _ _ Defined) = True
+defined _ = False
+
+es'' :: [(BNF, Integer, [Integer])]
+es'' = compile rules'
+
+ms'' :: Map Integer BNF'
+ms'' = inverseMap es''
+
+g' :: Int -> IO ()
+g' x = do 
+  xs <- sample' $ genAll es'' x
+  mapM_ (putStrLn . showTree) xs
+
+genAll :: [BNF'] -> Int -> Gen (Tree Res)
+genAll bs i = do
+  j <- elements (filter (\(_, key :: Integer, _) -> defined (rules' !! fromInteger key)) bs)
+  gen j ms'' i
+
+-- use `g' 10` to sample some examples
